7a45583fbbb8de73bd641f78337f477a5059bb80
Eugene Van Zyl <eugevanz@gmail.com>
2024-04-24 10:54:57 +0000

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from supabase import create_client, Client
+import os
+import random
 
 app = Flask(__name__)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 @app.route("/")
 def hello_world():
