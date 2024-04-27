88465dacf0b41e3e84b371ffcaa82185570e5b67
eugenevanzyl <eugevanz@gmail.com>
2024-04-27 12:52:14 +0200

    Working on the collections grid

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ from flask import Flask, render_template, request
 from supabase import create_client, Client
 import os
 import random
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.jinja_env.globals.update(get_current_page_name=lambda: request.path.split("/")[1:])
-app.jinja_env.filters['to_html'] = lambda text: BeautifulSoup(text, 'html.parser')
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-products = supabase.table("Products").select("fullCode", "productName", "brand", "images", "variants", "keywords", "description").execute()
+products = supabase.table("Products").select("fullCode", "productName", "brand", "images", "variants", "keywords",
+                                             "description").execute()
 product_fullCodes = [product["fullCode"] for product in products.data]
 prices = supabase.table("Prices").select("fullCode", "price").in_("fullCode", product_fullCodes).execute()
 
@@ -22,6 +21,7 @@ for product in products.data:
         code_size = variant["codeSize"]
         if code_size is not None:
             sizes[code_size] = sizes.get(code_size, 0) + 1
+
 
 @app.context_processor
 def utility_processor():
@@ -34,13 +34,16 @@ def utility_processor():
     sizes_ = dict(sorted(sizes.items()))
     return dict(menu_items=menu_items, prices=prices.data, sizes=sizes_)
 
+
 @app.route("/selected-pay-meth")
 def selected_pay_meth():
     return request.args.get("payment-method")
 
+
 @app.route("/selected-billing-address")
 def selected_billing_address():
     return request.args.get("billing-addr")
+
 
 @app.route("/")
 def home():
@@ -48,17 +51,27 @@ def home():
     collections = [{"label": "Collection Name"}]
     return render_template("index.html", products=featured, collections=collections)
 
+
 @app.route("/Shop All")
 def shopall():
     page = request.args.get("page", 1, type=int)
     start_index = (page - 1) * 10
-    products = supabase.table("Products").select("simpleCode", "productName", "brand", "images").range(start_index, start_index + 10).execute()
+    paginated = supabase.table("Products").select("simpleCode", "productName", "brand", "images").range(start_index,
+                                                                                                        start_index + 10).execute()
     return render_template("products.html", products=products.data, page=page)
+
+
+@app.route("/Collections")
+def all_collections():
+    return render_template("collections.html")
+
 
 @app.route("/add-to-cart/<item_id>")
 def add_to_cart(item_id):
-    item = dict({"name": item_id, "image": "product.image", "price": "product.price", "quantity": 1, "size": "L", "color": "grey"})
+    item = dict({"name": item_id, "image": "product.image", "price": "product.price", "quantity": 1, "size": "L",
+                 "color": "grey"})
     return render_template("cart-confirmation.html", product=item)
+
 
 @app.route(rule="/buy-now", methods=["POST"])
 def buy_now():
@@ -69,16 +82,23 @@ def buy_now():
     selected_size = request.form["size"]
     selected_color = request.form["color"]
 
-    item = dict({"name": item_name, "image": item_image, "price": item_price, "quantity": selected_quantity, "size": selected_size, "color": selected_color})
+    item = dict({"name": item_name, "image": item_image, "price": item_price, "quantity": selected_quantity,
+                 "size": selected_size, "color": selected_color})
     return render_template("checkout.html", products=item)
+
 
 @app.route("/product-detail/<item_id>")
 def product_detail(item_id):
     product = products.data[0]
     return render_template("product-detail.html", product=product)
 
+
 @app.route("/description/<fullCode>")
-def item_desc(fullCode):
-    desc = next((product.get("description") for product in products.data if product.get("fullCode") == fullCode), None)
+def item_desc(full_code: str):
+    desc = next((product.get("description") for product in products.data if product.get("fullCode") == full_code), None)
     print(desc)
     return desc
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
