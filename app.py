198ad235da7eaccd1a591bc372ff141b01b9e67f
eugenevanzyl <eugevanz@gmail.com>
2024-06-27 20:59:12 +0200

    Using a feather file to pull data. Converted to a flask app from FastAPI. Need to adjust Render settings

--- app.py
+++ app.py
@@ -1,289 +1,155 @@
+import os
 import random
 
-from bs4 import BeautifulSoup
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
-from data import categories, products, banners, default_img, sizes, collections, brands, genders, materials, fits, \
-    keywords, random_products
+from data import banners, default_img, sizes, genders, materials, fits, \
+    keywords, brands, categories, colour_filter, avail_colours, avail_sizes, to_html_from_text, products_df, \
+    collections_df, paginate_dataframe, Pagination
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+database_uri = os.environ.get("DATABASE_URI")
 
-templates = Jinja2Templates(directory="templates")
-templates.env.globals["categories_4"] = random.sample(categories.data, 4)
-templates.env.filters["prettify_html"] = lambda html_content: BeautifulSoup(html_content,
-                                                                            features="html.parser").get_text()
+app = Flask(__name__)
 
-app.product_page = 0
-app.selected_sizes, app.selected_genders, app.selected_materials, app.selected_fits = [], [], [], []
-app.selected_brands, app.selected_keywords, app.filters = [], [], []
+app.config["SECRET_KEY"] = "omniscient"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+
+db = SQLAlchemy(app=app)
 
 
-def filter_store(selection_rem: str, selection_add: str, stored_filter: str):
-    if selection_add is not None:
-        if stored_filter == "size":
-            app.selected_sizes.append(selection_add)
-            app.selected_sizes = list(set(app.selected_sizes))
-        elif stored_filter == "gender":
-            app.selected_genders.append(selection_add)
-            app.selected_genders = list(set(app.selected_genders))
-        elif stored_filter == "material":
-            app.selected_materials.append(selection_add)
-            app.selected_materials = list(set(app.selected_materials))
-        elif stored_filter == "fit":
-            app.selected_fits.append(selection_add)
-            app.selected_fits = list(set(app.selected_fits))
-        elif stored_filter == "brand":
-            app.selected_brands.append(selection_add)
-            app.selected_brands = list(set(app.selected_brands))
-        elif stored_filter == "keyword":
-            app.selected_keywords.append(selection_add)
-            app.selected_keywords = list(set(app.selected_keywords))
-    if selection_rem is not None:
-        try:
-            if stored_filter == "size":
-                app.selected_sizes.remove(selection_rem)
-            elif stored_filter == "gender":
-                app.selected_genders.remove(selection_rem)
-            elif stored_filter == "material":
-                app.selected_materials.remove(selection_rem)
-            elif stored_filter == "fit":
-                app.selected_fits.remove(selection_rem)
-            elif stored_filter == "brand":
-                app.selected_brands.remove(selection_rem)
-            elif stored_filter == "keyword":
-                app.selected_keywords.remove(selection_rem)
-        except ValueError:
-            print(f"The element '{selection_rem}' is not in the list")
+class Product(db.Model):
+    __tablename__ = "Products"
+    simplecode = db.Column(db.String, primary_key=True)
+    fullcode = db.Column(db.String)
+    gender = db.Column(db.String)
+    material = db.Column(db.String)
+    fit = db.Column(db.String)
+    description = db.Column(db.String)
+    productname = db.Column(db.String)
+    keywords = db.Column(db.ARRAY(db.String))
+    islogo24 = db.Column(db.Boolean)
+    brandname = db.Column(db.String)
+    imagelist = db.Column(db.ARRAY(db.String))
+    category = db.Column(db.String)
+    codesizenames = db.Column(db.ARRAY(db.String))
+    codecolournames = db.Column(db.ARRAY(db.String))
+    price = db.Column(db.Float)
 
 
-def modify_filter(filter_key, new_value):
-    filter_dict = next((f for f in app.filters if filter_key in f), None)
-    if filter_dict is not None:
-        if new_value in filter_dict[filter_key]:
-            filter_dict[filter_key].remove(new_value)
-            print("filter", filter_key, "length", len(filter_dict[filter_key]))
-            if not len(filter_dict[filter_key]):
-                app.filters = [f for f in app.filters if filter_key not in f]
-        else:
-            filter_dict[filter_key].append(new_value)
-    else:
-        app.filters.append({filter_key: [new_value]})
+class Collections(db.Model):
+    __tablename__ = "Collections"
+    path = db.Column(db.String, primary_key=True)
+    code = db.Column(db.String)
+    image = db.Column(db.String)
+    name = db.Column(db.String)
 
 
-def apply_filters() -> list:
-    filtered_products = products.data
-    for filter_dict in app.filters:
-        for key, allowed_values in filter_dict.items():
-            if key == "keywords":
-                filtered_products = [product for product in filtered_products if allowed_values in product[key]]
-            # elif key == "size":
-            #     filtered_products = [product for product in filtered_products if allowed_values]
-            else:
-                filtered_products = [product for product in filtered_products if product[key] in allowed_values]
-    return filtered_products
+app.jinja_env.filters["prettify_html"] = to_html_from_text
+app.jinja_env.filters["colour_filter"] = colour_filter
+app.jinja_env.filters["avail_colours"] = avail_colours
+app.jinja_env.filters["avail_sizes"] = avail_sizes
 
 
-def colour_filter(colour: str) -> str:
-    if colour is None: return "#FFFAFA"
-    colours = {"PINK": "#FF69B4", "RED": "#DC143C", "YELLOW": "#FFD700", "PURPLE": "#BA55D3",
-               "STONE MILITARY GREEN": "#556B2F", "SKY BLUE": "#87CEEB", "DARK GREY": "#A9A9A9", "STONE": "#e3cba5",
-               "ROYAL BLUE": "#4169E1", "BLUE": "#0000FF", "WHITE": "#FAF9F6", "GREY": "#808080", "NAVY": "#000080",
-               "MILITARY GREEN": "#667C3E", "CHARCOAL": "#36454F", "BLACK": "#141414", "SOLID WHITE": "#FAF9F6",
-               "NATURAL": "#FAF9F6", "GOLD": "#FFD700", "SILVER": "#C0C0C0", "GUN METAL": "#818589", "KHAKI": "#F0E68C",
-               "TRANSPARENT": "#FAF9F6", "CYAN": "#7FFFD4", "LIME": "#32CD32", "GREEN": "#228B22", "ORANGE": "#FF7518",
-               "TURQUOISE": "#40E0D0", "DARK GREEN": "#097969", "ROSE GOLD": "#E0BFB8", "BRONZE": "#CD7F32",
-               "MAROON": "#800000", "AQUA": "#89CFF0", "OCEAN BLUE": "#0059b3", "CREAM": "#FFFDD0",
-               "LIGHT BLUE": "#89CFF0", "WHITE BLACK": "#FAF9F6", "WHITE LIGHT BLUE": "#F0FFFF", "OLIVE": "#808000",
-               "DARK BLUE": "#00008B", "BROWN OLD": "#6E260E", "WHITE NAVY": "#B6D0E2", "BROWN": "#8B4513",
-               "BLACK RED": "#FBFCFC", "Light Grey": "#d3d3d3", "NAVY NAVY": "#000080", "DARK RED": "#8B0000"}
-    return colours[colour]
+# def filter_products():
+#     query = Product.query
+#     for key, values in session.items():
+#         if values and hasattr(Product, key):
+#             column = getattr(Product, key)
+#             if isinstance(values, list):
+#                 if column.type.__class__.__name__ == 'ARRAY':
+#                     like_filters = [column.any(func.lower(element)).like(f"%{value.lower()}%") for value in values]
+#                 else:
+#                     like_filters = [column.like(f"%{value}%") for value in values]
+#                 query = query.filter(or_(*like_filters))
+#             else:
+#                 if column.type.__class__.__name__ == 'ARRAY':
+#                     query = query.filter(column.any(func.lower(values)).like(f"%{values.lower()}%"))
+#                 else:
+#                     query = query.filter(column.like(f"%{values}%"))
+#     return query
 
 
-templates.env.filters["colour_filter"] = colour_filter
+@app.context_processor
+def inject_shared_data():
+    return {"categories": random.sample(categories.data, 4)}
 
 
-def avail_colours(variants: list) -> list:
-    code_colour_names = []
-    for item in variants:
-        code_colour_names.append(item["codeColourName"])
-        code_colour_names = list(set(code_colour_names))
-    return code_colour_names
+@app.route("/")
+def index():
+    products_ = products_df.sample(n=15).to_dict(orient='records')
+    collections_ = collections_df.sample(n=15).to_dict(orient='records')
+    return render_template(template_name_or_list="index.html", banners=banners, products=products_,
+                           collections=collections_, brands=brands.data)
 
 
-templates.env.filters["avail_colours"] = avail_colours
+@app.route("/products/<int:page>/")
+def all_products(page: int = 1):
+    total_items = len(products_df)
+    paginated_df, total_pages = paginate_dataframe(products_df, page)
+    pagination_ = Pagination(page, len(products_df))
+    products_ = paginated_df.to_dict(orient='records')
+    return render_template("products.html", sizes=sizes, genders=genders, materials=materials,
+                           fits=fits, brands=[item["name"] for item in brands.data], keywords=keywords,
+                           products=products_, default_img=default_img, pagination=pagination_)
 
 
-def avail_sizes(variants: list) -> list:
-    code_size_names = []
-    for item in variants:
-        if item["codeSizeName"]:
-            code_size_names.append(item["codeSizeName"])
-        code_size_names = list(set(code_size_names))
-    return code_size_names
+@app.route("/product-filter/")
+def product_filter():
+    session["gender"] = request.args.getlist("gender")
+    session["size"] = request.args.getlist("size")
+    session["material"] = request.args.getlist("material")
+    session["fit"] = request.args.getlist("fit")
+    session["brand"] = request.args.getlist("brand")
+    session["keywords"] = request.args.getlist("keywords")
+
+    return redirect(url_for("all_products", page=1))
 
 
-templates.env.filters["avail_sizes"] = avail_sizes
+@app.route("/products-slider/")
+def products_slider():
+    return render_template("products-slider.html", banners=banners)
 
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    context = {
-        "request": request,
-        "banners": banners
-    }
-    return templates.TemplateResponse("index.html", context=context)
+@app.route("/products-grid/<int:page>", methods=['GET', 'POST'])
+def products_grid():
+    return render_template("products-grid.html")
 
 
-@app.get("/products", response_class=HTMLResponse)
-async def all_products(request: Request):
-    context = {
-        "request": request
-    }
-    return templates.TemplateResponse("products.html", context=context)
+# @app.route("/product-detail", methods=['GET', 'POST'])
+# def product_detail():
+#     form_data = request.form
+#     product_data = form_data.get("product")
+#     return render_template("product-detail.html", product=eval(product_data), recommended=random_products)
 
 
-@app.get("/products-slider", response_class=HTMLResponse)
-async def products_slider(request: Request):
-    context = {
-        "request": request,
-        "banners": banners
-    }
-    return templates.TemplateResponse("products-slider.html", context=context)
+@app.route("/collections/")
+def all_collections():
+    return render_template("collections.html")
 
 
-@app.get("/products-grid", response_class=HTMLResponse)
-async def products_grid(request: Request, page: int = None, is_feat: bool = False, gender: str = None, size: str = None,
-                        material: str = None, fit: str = None, brand: str = None, keyword: str = None):
-    if gender is not None: modify_filter(filter_key="gender", new_value=gender)
-    if size is not None: modify_filter(filter_key="size", new_value=size)
-    if material is not None: modify_filter(filter_key="material", new_value=material)
-    if fit is not None: modify_filter(filter_key="fit", new_value=fit)
-    if brand is not None: modify_filter(filter_key="brand", new_value=brand)
-    if keyword is not None: modify_filter(filter_key="keyword", new_value=keyword)
-    if page is not None: app.product_page = max(page, 1)  # If page is provided, set product_page
-    if app.product_page < 1:  app.product_page = 1  # Ensure product_page is always at least 1
-    start = (app.product_page - 1) * 15  # Calculate the start index for pagination
-    print(app.filters)
-    context = {  # Prepare context data for template rendering
-        "request": request,  # Request object
-        "page": app.product_page,  # Current page number
-        "default_img": default_img,  # Default image URL
-        "is_feat": is_feat,  # Flag indicating if only featured products are requested
-        "products": random_products.data if is_feat else apply_filters()[start: (start + 15)]
-    }
-    return templates.TemplateResponse("products-grid.html", context=context)
+@app.route("/collections-grid/")
+def collections_grid():
+    collections_ = Collections.query.order_by(func.random()).all()
+    return render_template("collections-grid.html", default_img=default_img, collections=collections_)
 
 
-@app.get("/product-filter", response_class=HTMLResponse)
-async def product_filter(request: Request, a_size: str = None, r_size: str = None, a_gender: str = None,
-                         r_gender: str = None, a_material: str = None, r_material: str = None, a_fit: str = None,
-                         r_fit: str = None, a_brand: str = None, r_brand: str = None, a_keyword: str = None,
-                         r_keyword: str = None):
-    filter_store(r_size, a_size, "size")
-    filter_store(r_gender, a_gender, "gender")
-    filter_store(r_material, a_material, "material")
-    filter_store(r_fit, a_fit, "fit")
-    filter_store(r_brand, a_brand, "brand")
-    filter_store(r_keyword, a_keyword, "keyword")
-    context = {
-        "request": request,
-        "sizes": sizes,
-        "selected_sizes": app.selected_sizes,
-        "genders": genders,
-        "selected_genders": app.selected_genders,
-        "materials": materials,
-        "selected_materials": app.selected_materials,
-        "fits": fits,
-        "selected_fits": app.selected_fits,
-        "brands": [item["name"] for item in brands.data],
-        "selected_brands": app.selected_brands,
-        "keywords": keywords,
-        "selected_keywords": app.selected_keywords
-    }
-    return templates.TemplateResponse("product-filter.html", context=context)
+@app.route("/suppliers-grid/")
+def suppliers_grid():
+    return render_template("suppliers-grid.html", brands=brands.data)
 
 
-@app.post("/product-detail", response_class=HTMLResponse)
-async def product_detail(request: Request):
-    form_data = await request.form()
-    product_data = form_data.get("product")
-    context = {
-        "request": request,
-        "product": eval(product_data),
-        "recommended": random_products.data
-    }
-    return templates.TemplateResponse("product-detail.html", context=context)
+# @app.route("/selected-pay-meth")
+# def selected_pay_meth(request: Request):
+#     return request.get("payment-method")
 
 
-@app.get("/collections", response_class=HTMLResponse)
-async def all_collections(request: Request):
-    context = {
-        "request": request
-    }
-    return templates.TemplateResponse("collections.html", context=context)
+# @app.get("/selected-billing-address")
+# def selected_billing_address(request: Request):
+#     return request.get("billing-addr")
 
 
-@app.get("/collections-grid", response_class=HTMLResponse)
-async def collections_grid(request: Request, is_feat: bool = False):
-    context = {
-        "request": request,
-        "default_img": default_img,
-        "collections": random.sample(collections, 15) if is_feat else collections
-    }
-    return templates.TemplateResponse("collections-grid.html", context=context)
-
-
-@app.get("/suppliers-grid", response_class=HTMLResponse)
-async def suppliers_grid(request: Request):
-    context = {
-        "request": request,
-        "brands": brands.data
-    }
-    return templates.TemplateResponse("suppliers-grid.html", context=context)
-
-
-@app.get("/selected-pay-meth")
-def selected_pay_meth(request: Request):
-    return request.get("payment-method")
-
-
-@app.get("/selected-billing-address")
-def selected_billing_address(request: Request):
-    return request.get("billing-addr")
-
-# @app.route("/add-to-cart/<item_id>")
-# def add_to_cart(item_id):
-#     item = dict({"name": item_id, "image": "product.image", "price": "product.price", "quantity": 1, "size": "L",
-#                  "color": "grey"})
-#     return render_template("cart-confirmation.html", product=item)
-
-# @app.route(rule="/buy-now", methods=["POST"])
-# def buy_now():
-#     item_name = request.form["name"]
-#     item_image = request.form["image"]
-#     item_price = request.form["price"]
-#     selected_quantity = int(request.form["quantity"])
-#     selected_size = request.form["size"]
-#     selected_color = request.form["color"]
-#
-#     item = dict({"name": item_name, "image": item_image, "price": item_price, "quantity": selected_quantity,
-#                  "size": selected_size, "color": selected_color})
-#     return render_template("checkout.html", products=item)
-
-# @app.route("/product-detail/<item_id>")
-# def product_detail(item_id):
-#     product = products.data[0]
-#     return render_template("product-detail.html", product=product)
-
-# @app.route("/description/<fullCode>")
-# def item_desc(full_code: str):
-#     desc = next((product.get("description") for product in products.data if product.get("fullCode") == full_code), None)
-#     print(desc)
-#     return desc
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- data.py
+++ data.py
@@ -1,33 +1,121 @@
 import os
 
+import pandas as pd
+from bs4 import BeautifulSoup
 from supabase import create_client, Client
 from supafunc.errors import FunctionsRelayError, FunctionsHttpError
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
+password: str = os.environ.get("SUPABASE_PASS")
 supabase: Client = create_client(url, key)
 
-random_products = supabase.table("random_products").select("*").limit(15).execute()
 
-selected_product = lambda code: supabase.table("Products").select(
-    "fullCode", "productName", "brand", "images", "variants", "keywords", "description", "categories", "gender",
-    "material", "fit"
-).eq("fullCode", code).execute()
+class Pagination:
+    def __init__(self, page, total_items):
+        self.page = page
+        self.per_page = 15
+        self.total_items = total_items
+        self.total_pages = (total_items // 15) + (1 if total_items % 15 > 0 else 0)
 
-products = supabase.table("Products").select(
-    "fullCode", "productName", "brand", "images", "variants", "keywords", "description", "categories", "gender",
-    "material", "fit"
-).execute()
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def prev_num(self):
+        return self.page - 1 if self.has_prev else None
+
+    @property
+    def has_next(self):
+        return self.page < self.total_pages
+
+    @property
+    def next_num(self):
+        return self.page + 1 if self.has_next else None
+
+    def iter_pages(self, left_edge=2, left_current=2, right_current=2, right_edge=2):
+        last = 0
+        for num in range(1, self.total_pages + 1):
+            pg__left_current = self.page - left_current - 1
+            pg__right_current = self.page + right_current
+            pg__right_edge = self.total_pages - right_edge
+            if num <= left_edge or pg__left_current < num < pg__right_current or num > pg__right_edge:
+                if last + 1 != num: yield None
+                yield num
+                last = num
+
+
+def paginate_dataframe(df, page):
+    total_items = len(df)
+    per_page = 15
+    total_pages = (total_items // per_page) + (1 if total_items % per_page > 0 else 0)
+
+    if page < 1 or page > total_pages: return pd.DataFrame(), 0
+
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_df = df.iloc[start:end]
+
+    return paginated_df, total_pages
+
+
+def download_and_read_feather(bucket_name: str, source: str, destination: str):
+    with open(destination, 'wb+') as f:
+        res = supabase.storage.from_(bucket_name).download(source)
+        f.write(res)
+    return pd.read_feather(destination)
+
+
+products_df = download_and_read_feather(
+    bucket_name='public', source='Dataframes/products.feather', destination='products.feather')
+
+collections_df = download_and_read_feather(
+    bucket_name='public', source='Dataframes/collections.feather', destination='collections.feather')
+
+
+def avail_sizes(variants: list) -> list:
+    code_size_names = []
+    for item in variants:
+        if item["codeSizeName"]:
+            code_size_names.append(item["codeSizeName"])
+        code_size_names = list(set(code_size_names))
+    return code_size_names
+
+
+def avail_colours(variants: list) -> list:
+    code_colour_names = []
+    for item in variants:
+        code_colour_names.append(item["codeColourName"])
+        code_colour_names = list(set(code_colour_names))
+    return code_colour_names
+
+
+def colour_filter(colour: str) -> str:
+    if colour is None: return "#FFFAFA"
+    colours = {"PINK": "#FF69B4", "RED": "#DC143C", "YELLOW": "#FFD700", "PURPLE": "#BA55D3",
+               "STONE MILITARY GREEN": "#556B2F", "SKY BLUE": "#87CEEB", "DARK GREY": "#A9A9A9", "STONE": "#e3cba5",
+               "ROYAL BLUE": "#4169E1", "BLUE": "#0000FF", "WHITE": "#FAF9F6", "GREY": "#808080", "NAVY": "#000080",
+               "MILITARY GREEN": "#667C3E", "CHARCOAL": "#36454F", "BLACK": "#141414", "SOLID WHITE": "#FAF9F6",
+               "NATURAL": "#FAF9F6", "GOLD": "#FFD700", "SILVER": "#C0C0C0", "GUN METAL": "#818589", "KHAKI": "#F0E68C",
+               "TRANSPARENT": "#FAF9F6", "CYAN": "#7FFFD4", "LIME": "#32CD32", "GREEN": "#228B22", "ORANGE": "#FF7518",
+               "TURQUOISE": "#40E0D0", "DARK GREEN": "#097969", "ROSE GOLD": "#E0BFB8", "BRONZE": "#CD7F32",
+               "MAROON": "#800000", "AQUA": "#89CFF0", "OCEAN BLUE": "#0059b3", "CREAM": "#FFFDD0",
+               "LIGHT BLUE": "#89CFF0", "WHITE BLACK": "#FAF9F6", "WHITE LIGHT BLUE": "#F0FFFF", "OLIVE": "#808000",
+               "DARK BLUE": "#00008B", "BROWN OLD": "#6E260E", "WHITE NAVY": "#B6D0E2", "BROWN": "#8B4513",
+               "BLACK RED": "#FBFCFC", "Light Grey": "#d3d3d3", "NAVY NAVY": "#000080", "DARK RED": "#8B0000",
+               "BLACK GREY": "#3F3F3F", "BLACK CYAN": "#008B8B", "BEIGE": "#FAF0E6", "BLACK WHITE": "#FAF9F6",
+               "NAVY LIGHT BLUE": "#89CFF0", "BRIGHT GREEN": "#66FF00", "CAMOUFLAGE": "#78866B", "GREY LIME":
+                   "#9DFD38", "GREEN GOLD": "#d8bb78", "BLACK ORANGE": "#FF8C00", "BLACK YELLOW": "#e1d816",
+               "NATURAL BROWN": "#A27C5B"}
+    return colours[colour]
+
+
+def to_html_from_text(html_content: str):
+    return BeautifulSoup(html_content, features="html.parser").get_text()
+
 
 brands = supabase.table("Brands").select("code", "name", "image").execute()
-
-product_fullCodes = [product["fullCode"] for product in products.data]
-prices = supabase.table("Prices").select("fullCode", "price").in_("fullCode", product_fullCodes).execute()
-price_lookup = {price["fullCode"]: price["price"] for price in prices.data}
-for product in products.data:
-    full_code = product["fullCode"]
-    if full_code in price_lookup:
-        product["price"] = price_lookup[full_code]
 
 banners = [
     "https://inyllwqsghzahwjosayv.supabase.co/storage/v1/object/public/Banners/PFESSC%20Banners.001.png",
@@ -48,11 +136,5 @@ try:
 except (FunctionsRelayError, FunctionsHttpError) as exception:
     err = exception.to_dict()
     print(err.get("message"))
-
-collections = set()
-for product in products.data:
-    for category in product["categories"]:
-        collections.add((category["code"], category["name"], category["path"], category["image"]))
-collections = [{"code": code, "name": name, "path": path, "image": image} for code, name, path, image in collections]
 
 default_img = "https://inyllwqsghzahwjosayv.supabase.co/storage/v1/object/public/Misc/branding-5_eba963a2-dc8f-47fe-b495-91101e675608.png?t=2024-05-07T06%3A14%3A55.152Z"
